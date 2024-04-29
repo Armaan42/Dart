dd2171106d4e9a9f61a2200c403f4b69651b8860
Armaan <armaansinghbhau8@gmail.com>
2024-04-29 12:00:42 +0530

    Added switch case

---
+++ switch_case.dart
@@ -0,0 +1,14 @@
+void main(){
+  var vehicle = "car";
+
+  switch(vehicle){
+    case 'car':
+      print("good boy");break;
+    case 'truck':
+      print("bad boy");break;
+    case 'bike':
+      print("cool boy");break;
+    default:
+      print("naughty boy");
+  }
+}
