0db8e456d46681181852a2a1d6cbc3f7bc7c4850
Armaan <armaansinghbhau8@gmail.com>
2024-04-29 11:26:28 +0530

    different type of loops

---
+++ loops/do_while_loop.dart
@@ -0,0 +1,8 @@
+void main(){
+  int i = 0;
+  do{
+    print("hello");
+    i++;
+  }
+  while(i < 5);
+}
---
+++ loops/for_loop.dart
@@ -0,0 +1,9 @@
+void main(){
+  for(int i=0; i<2; i++){
+        print("The value of i = $i");
+    for(int j=0; j<2; j++){
+        print("j = $j");
+    }
+    print("");
+  }
+}
