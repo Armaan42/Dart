72e85a39a7af161bade4a351f8d0dc62d5e4c78b
Armaan <armaansinghbhau8@gmail.com>
2024-04-30 20:19:23 +0530

    short hand notation expression

---
+++ shorthand_syntax_expression.dart
@@ -0,0 +1,9 @@
+
+//for functions that contains just one expression, you can use a shorthand syntax.
+// The => notation is sometimes referred to as arrow syntax
+void main(){
+  print(add(10,20));
+}
+
+int add(var a, var b) => a+b;
+
