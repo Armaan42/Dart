3853a4d269f74a1cecdb2bdb559fe7a45cf41ac7
Armaan <armaansinghbhau8@gmail.com>
2024-04-30 21:02:01 +0530

    Required parameters

---
+++ parameters/required_parameters.dart
@@ -0,0 +1,8 @@
+void main(){
+  student("John", 1); // In this both the parameters is required otherwise this is will give us error.
+}
+
+void student(String name, int roll){
+  print(name);
+  print(roll); 
+}
