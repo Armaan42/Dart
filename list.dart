02d2a4afe57937c460f5a368e99f11ada9beede1
Armaan <armaansinghbhau8@gmail.com>
2024-05-01 17:54:48 +0530

    Added list

---
+++ list.dart
@@ -0,0 +1,11 @@
+void main(){
+  List<int> lst = [10, 20, 30]; //integer list
+  print(lst);
+  print(lst[1]);
+
+  List <String> lst1 = ["aaa", "bbb", "CCC"]; //string list
+  print(lst1);
+
+  List <double> lst2 = [1.1, 2.3]; //double list
+  print(lst2);
+}
