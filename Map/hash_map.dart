0380c837834b4474cde6e931fef097c01d95a313
Armaan <armaansinghbhau8@gmail.com>
2024-05-01 21:50:49 +0530

    hash_map.dart

---
+++ Map/hash_map.dart
@@ -0,0 +1,19 @@
+import 'dart:collection';
+
+void main(){
+  var map1 = HashMap();
+
+  map1[1] = 10;
+  map1[2] = 20;
+  map1[3] = 30;
+
+  print(map1);
+
+  for(int i in map1.keys){
+    print(i);
+  }
+  print("");
+  for(int i in map1.keys){
+    print(i);
+  }
+}
