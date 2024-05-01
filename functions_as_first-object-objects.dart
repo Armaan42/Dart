f63b90138fd18eee1af74d14309e7910af508cfb
Armaan <armaansinghbhau8@gmail.com>
2024-05-01 17:35:41 +0530

    function as first object

---
+++ functions_as_first-object-objects.dart
@@ -0,0 +1,14 @@
+// You can pass a function as a parameter to another function.
+
+void main(){
+  student("Hello", name);
+}
+
+Function name = (String s){
+  return s;
+};
+
+void student(String a, Function n){
+  print('$a ${n('John')}');
+}
+
