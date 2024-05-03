9a05dad9d55b6a5825f0245ab63aef51d44bad53
Armaan <armaansinghbhau8@gmail.com>
2024-05-03 10:03:12 +0530

    class demo

---
+++ class/class_demo.dart
@@ -0,0 +1,15 @@
+// Defining a class named Pet
+class Pet {
+  // Method to display a greeting
+  void display() {
+    print("Hello");
+  }
+}
+
+// Main function
+void main() {
+  // Creating an object of the Pet class
+  var obj = Pet();
+  // Calling the display method of the object
+  obj.display();
+}
